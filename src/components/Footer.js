6f64c1358f2ff85c542306c16e57c722db8d5181
Martha Mandizvidza <marthagmandizvidza60@gmail.com>
2020-05-13 21:55:57 +0100

    'footer'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,56 @@
 import React from "react";
+
 /*import "../css/footer.css";*/
 
 function Footer() {
   return (
     <div>
       <div className="footer">
-        <div className="copyrights">
-          <span className="copyright">
-            &copy; 2020 by Martha G Mandizvidza.
-          </span>
-        </div>
         <div className="social">
-          <span className="flink">FACEBOOK</span>
-          <span className="flink">TWITTER</span>
-          <span className="flink">EMAIL</span>
-          <span className="flink">LINKEDIN</span>
+          <div className="footerWrapper">
+            <div className="socialnner">
+              <span className="flink">
+                <a
+                  href="https://www.facebook.com/marthag.mandizvidza/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  FACEBOOK
+                </a>
+              </span>
+
+              {/*  <span className="flink">
+              {" "}
+              <a href='className="social-links"'>EMAIL</a>
+            </span>*/}
+              <span className="flink">
+                <a
+                  href="https://github.com/MarthaGamu"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  GITHUB
+                </a>
+              </span>
+              <span className="flink">
+                <a
+                  href="https://www.linkedin.com/in/martha-g-mandizvidza-131956131/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  LINKEDIN
+                </a>
+              </span>
+              <span className="flink">
+                <a href="mailto:marthagmandizvidza60@gmail.com">EMAIL</a>
+              </span>
+            </div>
+            <div className="copyrights">
+              <span className="copyright">
+                &copy; 2020 by Martha G Mandizvidza.
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
